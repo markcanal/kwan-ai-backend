f03a80ea57dc93a641d6e1402ffc29faedcfc4fc
Mark Jerald Canal <markjeraldc@gmail.com>
2025-11-02 15:36:42 +0800

    Initial Kwan-ai backend

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
 import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
+
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   providers: [AttendanceService],
   controllers: [AttendanceController],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { DatabaseModule } from '../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   providers: [UserService],
   controllers: [UserController],
 })
