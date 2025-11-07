a0aa041f338aa1d9ebd7fc8bb20666ca680372a6
Mark <markjeraldc@gmail.com>
2025-11-07 21:41:35 +0800

    Feature/job service and email templating (#2)
    * Initial Kwan-ai backend
    
    * Implement Resend sending email and template
    
    - Created
    -Payroll cut-off
    -Welcome email

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -7,11 +7,34 @@ export class JobsService {
   private queue: Queue;
   constructor() {
     const connection = new IORedis(process.env.REDIS_URL);
+    connection.on('connect', () => console.log('✅ Redis connected'));
+    connection.on('error', (err) => console.error('❌ Redis connection error:', err));
+
     this.queue = new Queue('emails', { connection });
   }
 
-  async enqueuePayrollEmail(data: any) {
+  async enqueuePayrollEmail(data: {
+    to: string;
+    name: string;
+    daysWorked: number;
+    totalHours: number;
+    baseSalary: number;
+    overtimeHours: number;
+    overtimeRate: number;
+    nightDiff: number;
+    sss: number;
+    philhealth: number;
+    pagibig: number;
+  }) {
     await this.queue.add('payroll-cutoff', data);
+    return { ok: true };
+  }
+
+  async enqueueWelcomeEmail(data: any) {
+    await this.queue.add('welcome-email', {
+      to: data.to,
+      name: data.name,
+    });
     return { ok: true };
   }
 }
