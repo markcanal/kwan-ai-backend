6db94f37a1838d5e6028333e9c8c581f39ce4e93
Mark Jerald Canal <markjeraldc@gmail.com>
2025-11-08 14:04:05 +0800

    Add userservice provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { FirebaseService } from './firebase.service';
 import { AuthService } from './auth.service';
 import { FirebaseAuthGuard } from './auth.guard';
 import { AuthController } from './auth.controller';
+import { UserService } from '../user/user.service';
 
 @Module({
-  providers: [FirebaseService, AuthService, FirebaseAuthGuard],
+  providers: [FirebaseService, AuthService, FirebaseAuthGuard, UserService],
   exports: [AuthService, FirebaseService, FirebaseAuthGuard],
   controllers: [AuthController],
+
 })
 export class AuthModule {}
