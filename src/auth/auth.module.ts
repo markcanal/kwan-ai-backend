22ab30a6dabc612418ae62042d625d24db6ceb53
Mark <markjeraldc@gmail.com>
2025-11-08 13:39:53 +0800

    Add auth sync endpoint (#5)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { FirebaseService } from './firebase.service';
 import { AuthService } from './auth.service';
 import { FirebaseAuthGuard } from './auth.guard';
+import { AuthController } from './auth.controller';
 
 @Module({
   providers: [FirebaseService, AuthService, FirebaseAuthGuard],
   exports: [AuthService, FirebaseService, FirebaseAuthGuard],
+  controllers: [AuthController],
 })
 export class AuthModule {}
